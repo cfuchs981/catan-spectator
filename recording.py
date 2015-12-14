3d48d85ca9d2b32cf5d7d0593befbd2822d27453
Ross Anderson <ross.anderson@ualberta.ca>
2015-12-14 14:18:29 -0700

    add GameRecord docstrings

--- recording.py
+++ recording.py
@@ -2,6 +2,15 @@ import datetime
 
 
 class GameRecord(object):
+    """
+    class GameRecord introduces a machine-parsable, human-readable record of all actions made in a game of Catan.
+
+    Each record contains all publicly known information in the game.
+    Each record is sufficient to 'replay' a game.
+
+    Use #dump to get the record as a string.
+    Use #flush to write the record to a file.
+    """
     version = '0.0.1'
 
     def __init__(self, directory):
@@ -10,12 +19,27 @@ class GameRecord(object):
         self.timestamp = datetime.datetime.now()
 
     def record(self, content):
+        """
+        Writes a string to the record
+        """
         self.record_str += content
 
     def recordln(self, content):
+        """
+        Writes a string to the record, appending a newline
+        """
         self.record_str += '{0}\n'.format(content)
 
+    def dump(self):
+        """
+        Dumps the current record to a string, and returns it
+        """
+        return self.record_str
+
     def flush(self):
+        """
+        Writes the current record to a file, and returns the file descriptor
+        """
         # write to timestamp file in self.directory
         pass
 
@@ -28,71 +52,70 @@ class GameRecord(object):
         self._set_board_ports(ports)
         self.record('...CATAN!\n\n')
 
-    '''
-    $color rolls $number
-    '''
     def record_roll(self, player, roll):
-
+        """
+        $color rolls $number
+        """
         self.recordln('{0} rolls {1}'.format(player.color, roll))
 
-    '''
-    $color is robbed
-    '''
     def record_is_robbed(self, player):
+        """
+        $color is robbed
+        """
         self.recordln('{0} is robbed'.format(player.color))
 
-    '''
-    $color moves robber to $hex, steals from $color
-    '''
     def record_moves_robber_and_steals(self, player, tile_id, victim):
+        """
+        $color moves robber to $hex, steals from $color
+        """
         self.recordln('{0} moves robber to {1}, steals from {2}'.format(
             player.color,
             tile_id,
             victim.color
         ))
 
-    '''
-    $color buys settlement, builds at $location
-    '''
     def record_buys_settlement(self, player, node_id):
+        """
+        $color buys settlement, builds at $location
+        """
         self.recordln('{0} buys settlement, builds at {1}'.format(
             player.color,
             node_id
         ))
 
-    '''
-    $color buys city, builds at $location
-    '''
     def record_buys_city(self, player, node_id):
+        """
+        $color buys city, builds at $location
+        """
         self.recordln('{0} buys city, builds at {1}'.format(
             player.color,
             node_id
         ))
 
-    '''
-    $color buys dev card
-    '''
     def record_buys_dev_card(self, player):
+        """
+        $color buys dev card
+        """
         self.recordln('{0} buys dev card'.format(
             player.color
         ))
 
-    '''
-    $color buys road, builds from $location to $location
-    '''
     def record_buys_road(self, player, node_id_1, node_id_2):
+        """
+        $color buys road, builds from $location to $location
+        """
         self.recordln('{0} buys road, builds from {1} to {2}'.format(
             player.color,
             node_id_1,
             node_id_2
         ))
 
-    '''
-    $color trades $number $resources[, $number resources]* to port:$port for $number $resources[, $number resources]*
-
-    the to_resources params are dicts of form {'wood':2,'brick':1}
-    '''
     def record_trade_with_port(self, player, to_port, port, to_player):
+        """
+        $color trades $number $resources[, $number resources]* to port:$port for $number $resources[, $number resources]*
+
+        the to_resources params are dicts of form {'wood':2,'brick':1}
+        """
         self.record('{0} trades '.format(player.color))
 
         # to_other items
@@ -115,12 +138,12 @@ class GameRecord(object):
 
         self.record('\n')
 
-    '''
-    $color trades [$number $resources, $number resources] to player:$color for [$number $resources, $number resources]
-
-    the to_resources params are dicts of form {'wood':2,'brick':1}
-    '''
     def record_trade_with_player(self, player, to_other, other, to_player):
+        """
+        $color trades [$number $resources, $number resources] to player:$color for [$number $resources, $number resources]
+
+        the to_resources params are dicts of form {'wood':2,'brick':1}
+        """
         self.record('{0} trades '.format(player.color))
 
         # to_other items
@@ -143,37 +166,37 @@ class GameRecord(object):
 
         self.record('\n')
 
-    '''
-    $color plays dev card: knight on $hex, steals from $color
-    '''
     def record_plays_dev_knight(self, player, tile_id, victim):
+        """
+        $color plays dev card: knight on $hex, steals from $color
+        """
         self.recordln('{0} plays dev card: knight on {1}, steals from {2}'.format(
             player.color,
             tile_id,
             victim.color
         ))
 
-    '''
-    $color plays dev card: monopoly on $resource
-
-    resource is the lowercase fulltext, eg 'wood', 'wheat', 'brick', 'ore', 'sheep'
-    '''
     def record_plays_dev_monopoly(self, player, resource):
+        """
+        $color plays dev card: monopoly on $resource
+
+        resource is the lowercase fulltext, eg 'wood', 'wheat', 'brick', 'ore', 'sheep'
+        """
         self.recordln('{0} plays dev card: monopoly on {1}'.format(
             player.color,
             resource
         ))
 
-    '''
-    $color plays dev card: victory point
-    '''
     def record_plays_dev_victory_point(self, player):
+        """
+        $color plays dev card: victory point
+        """
         self.recordln('{0} plays dev card: victory point'.format(player.color))
 
-    '''
-    $color plays dev card: road builder, builds from $location to $location and $location to $location
-    '''
     def record_plays_dev_road_builder(self, player, node_id_a1, node_id_a2, node_id_b1, node_id_b2):
+        """
+        $color plays dev card: road builder, builds from $location to $location and $location to $location
+        """
         self.recordln('{0} plays dev card: road builder, builds from {1} to {2} and {3} to {4}'.format(
             player.color,
             node_id_a1,
@@ -182,10 +205,10 @@ class GameRecord(object):
             node_id_b2
         ))
 
-    '''
-    $color ends turn
-    '''
     def record_ends_turn(self, player):
+        """
+        $color ends turn
+        """
         self.recordln('{0} ends turn'.format(player.color))
 
     def record_win(self, player):

dd2ffa7872122e77bc5c2387e8a406dc01c53433
Ross Anderson <ross.anderson@ualberta.ca>
2015-12-27 13:16:18 -0800

    using grid, not working yet

--- main.py
+++ main.py
@@ -28,21 +28,16 @@ class CatanSpectator(tkinter.Frame):
         self.game.observers.add(self)
         self._in_game = self.game.state.is_in_game()
 
-        self.left_frame = tkinter.Frame()
-        self.right_frame = tkinter.Frame()
-
-        self._board_frame = views.BoardFrame(self.left_frame, self.game)
-        self._log_frame = views.LogFrame(self.left_frame, self.game)
-        self._board_frame.pack(side=tkinter.TOP, fill=tkinter.Y)
-        self._log_frame.pack(side=tkinter.BOTTOM, fill=tkinter.X)
-        self.left_frame.pack(side=tkinter.LEFT)
+        self._board_frame = views.BoardFrame(self, self.game)
+        self._log_frame = views.LogFrame(self, self.game)
+        self._board_frame.grid(row=0, column=0)
+        self._log_frame.grid(row=1, column=0, sticky=tkinter.W)
 
         self._board_frame.redraw()
 
-        self._setup_game_toolbar_frame = views.SetupGameToolbarFrame(self.right_frame, self.game)
+        self._setup_game_toolbar_frame = views.SetupGameToolbarFrame(self, self.game)
         self._toolbar_frame = self._setup_game_toolbar_frame
-        self._toolbar_frame.pack(side=tkinter.TOP, fill=tkinter.BOTH)
-        self.right_frame.pack(side=tkinter.RIGHT)
+        self._toolbar_frame.grid(row=0, column=1, rowspan=2, sticky=tkinter.N)
 
     def notify(self, observable):
         was_in_game = self._in_game
@@ -51,12 +46,12 @@ class CatanSpectator(tkinter.Frame):
             # we were in game, now we're not
             self._toolbar_frame.pack_forget()
             self._toolbar_frame = self._setup_game_toolbar_frame
-            self._toolbar_frame.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+            self._toolbar_frame.grid(column=1)
         elif not was_in_game and self.game.state.is_in_game():
             # we were not in game, now we are
             self._toolbar_frame.pack_forget()
-            self._toolbar_frame = views.GameToolbarFrame(self.right_frame, self.game)
-            self._toolbar_frame.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+            self._toolbar_frame = views.GameToolbarFrame(self, self.game)
+            self._toolbar_frame.grid(column=1)
 
     def setup_options(self):
         return self._setup_game_toolbar_frame.options.copy()
